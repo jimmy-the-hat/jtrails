c29edf817fc28665083a29eb1b25a459097ecd7f
James Taylorson <jamestaylorson@gmail.com>
2013-04-03 22:13:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jtrails::Application.config.secret_token = '7d667d61e8f2fc2374b0507a882c6c26a8114fd82219b990d053e58c5a64b2d6a8dce1dbe268f906614fc333052370b446142d9746a76ede0789bcc9eafed248'
